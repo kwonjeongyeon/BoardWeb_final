361bc730daf9fe6ea9945ab4a574486f15df772c
jeongyeon <wjddus3159@naver.com>
2023-04-26 13:12:50 +0900

    2023.04.26 어노테이션 기반 MVC 개발(1) 1. 어노테이션 관련 설정 : HandlerMapping, Controller, ViewResolver 클래스를 <context:component-scan> 엘리먼트로 대체
    2. @Controller 사용하기
    : 어노테이션을 사용하면 컨트롤러 클래스를 일일이 <bean> 등록하지 않고 클래스 선언부 위에 "@Controller"를 붙이면 된다.
    *스프링 프레임워크가 지향하는 POJO(Plain Old Java Object) 클래스를 구현하려면 클래스 선언부의 "implements Controller"를 제거하고
    클래스 위에 "@Controller"를 선언해야 한다. (InsertBoardController.java 참고)
    ** InsertBoardController가 POJO 클래스로 변경되었으므로 메소드 이름을 insertBoard, 리턴타입을 void, 매개변수를 HttpServletRequest로 변경가능
    
    3.  @RequestMapping 사용하기
    : insertBoard() 메소드 위에 설정
    
    4. 클라이언트 요청 처리
    : 대부분 Controller는 사용자의 입력 정보를 추출하여 VO(Value Object) 객체에 저장, 비즈니스 컴포넌트의 메소드를 호출할 때 VO 객체를 인자로 전달
    **Command 객체 이용 : Controller 메소드 매개변수로 받은 VO 객체 (INserBoardController 클래서의 insertBoard() 메소드를 Command 객체 이용해 구현)
    : 사용자 입력 정보 추출, VO 객체 생성, 값 설정을 모두 컨테이너가 자동으로 처리 (Command 객체 생성하여 넘길 때 )
    * Form 태그 안의 파라미터 이름과 Command 객체의 Setter 메서드 이름이 반드시 일치해야 한다.

--- src/main/java/com/myspring/view/board/InsertBoardController.java
+++ src/main/java/com/myspring/view/board/InsertBoardController.java
@@ -1,40 +1,47 @@
 package com.myspring.view.board;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//import org.springframework.web.servlet.mvc.Controller;
 
 import com.myspring.biz.board.BoardVO;
 import com.myspring.biz.board.impl.BoardDAO;
 
-public class InsertBoardController implements Controller {
+@Controller
+public class InsertBoardController{
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+//	@Override
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	
+	@RequestMapping(value = "/insertBoard.do")
+	public void insertBoard(BoardVO vo) {
+		
+//	public void insertBoard(HttpServletRequest request) {
 		System.out.println("글 등록 처리");
 
-		// 1. 사용자 입력 정보 추출
-		// request.setCharacterEncoding("UTF-8");
-		String title = request.getParameter("title");
-		String writer = request.getParameter("writer");
-		String content = request.getParameter("content");
-
-		// 2. DB 연동 처리
-		BoardVO vo = new BoardVO();
-		vo.setTitle(title);
-		vo.setWriter(writer);
-		vo.setContent(content);
+//		// 1. 사용자 입력 정보 추출
+//		// request.setCharacterEncoding("UTF-8");
+//		String title = request.getParameter("title");
+//		String writer = request.getParameter("writer");
+//		String content = request.getParameter("content");
+//
+//		// 2. DB 연동 처리
+//		BoardVO vo = new BoardVO();
+//		vo.setTitle(title);
+//		vo.setWriter(writer);
+//		vo.setContent(content);
 
 		BoardDAO boardDAO = new BoardDAO();
 		boardDAO.insertBoard(vo);
 
-		// 3. 화면 네비게이션
-		ModelAndView mav = new ModelAndView();
-//		mav.setViewName("getBoardList.do");
-		mav.setViewName("redirect:getBoardList.do");
-		return mav;
+//		// 3. 화면 네비게이션
+//		ModelAndView mav = new ModelAndView();
+////		mav.setViewName("getBoardList.do");
+//		mav.setViewName("redirect:getBoardList.do");
+//		return mav;
 	}
 
 }
